957a83359e17354573e6d01678ef1f6eca4ef580
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2021-11-11 13:28:48 -0700

    completed all tasks

--- index.js
+++ index.js
@@ -86,14 +86,14 @@ class Car {
     return (this.tank += gallons);
   }
   drive(distance) {
-    if (this.tank * this.milesPerGallon > distance) {
-      return (this.odometer += distance);
-      // this.tank -= distance;
+    if (this.tank * this.milesPerGallon >= distance) {
+      this.odometer += distance;
+      this.tank -= distance / this.milesPerGallon;
+    } else {
+      this.odometer += this.tank * this.milesPerGallon;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
     }
-    // else {
-    //   const milesRemaining = distance - (this.tank * this.milesPerGallon)
-    //   if (milesRemaining
-    // }
   }
 }
 
